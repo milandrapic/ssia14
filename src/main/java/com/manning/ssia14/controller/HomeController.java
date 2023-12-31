11a44539d7fc62edf06497513268304f39604aca
milandrapic <milandrapic@gmail.com>
2023-12-31 15:53:40 -0500

    init commit

---
+++ src/main/java/com/manning/ssia14/controller/HomeController.java
@@ -0,0 +1,48 @@
+//
+//package com.manning.ssia14.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.Mapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.security.MessageDigest;
+//import java.security.NoSuchAlgorithmException;
+//import java.security.SecureRandom;
+//import java.util.Base64;
+//
+//@RestController
+//public class HomeController {
+//
+//    @GetMapping("/")
+//    public String hello(){
+//        return "hello";
+//    }
+//
+//    @GetMapping("/pkce")
+//    public String getChallengeAndVerifier() {
+//        SecureRandom secureRandom = new SecureRandom();
+//        byte[] code = new byte[32];
+//
+//        secureRandom.nextBytes(code);
+//
+//        String codeVerifier = Base64.getUrlEncoder()
+//                .withoutPadding()
+//                .encodeToString(code);
+//        try{
+//            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+//
+//            byte[] digested = messageDigest.digest(codeVerifier.getBytes());
+//            String codeChallenge = Base64.getUrlEncoder()
+//                    .withoutPadding()
+//                    .encodeToString(digested);
+//
+//            return String.format("Verifier: %s\nChallenge: %s\n", codeVerifier, codeChallenge);
+//        }
+//        catch(Exception e) {
+//            return "error";
+//        }
+//
+//    }
+//}
---
+++ src/test/java/com/manning/ssia14/Ssia14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manning.ssia14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssia14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
